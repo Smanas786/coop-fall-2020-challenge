07553eabe8f21e4c9ff12dcf242339aabd2078d0
Smanas <42001169+Smanas786@users.noreply.github.com>
2020-07-10 15:00:32 -0400

    Add files via upload
    shah1ag@uwidnsor.ca

--- solution_python.py
+++ solution_python.py
@@ -1,23 +1,65 @@
-class EventSourcer():
-    # Do not change the signature of any functions
+class Stack:
+     def __init__(self):
+         self.items = []
 
+     def isEmpty(self):
+         return self.items == []
+
+     def push(self, item):
+         self.items.append(item)
+
+     def pop(self):
+         return self.items.pop()
+
+     def peek(self):
+         return self.items[len(self.items)-1]
+
+     def size(self):
+         return len(self.items)
+    
+class EventSourcer():
+    # Do not change the signature of any functions    
+    
     def __init__(self):
         self.value = 0
+        self.state = 0
 
-    def add(self, num: int):
-        pass
+    def add(self, num: int):        
+        self.value += num
+        s.push(self.value)
+        return self.value
 
     def subtract(self, num: int):
-        pass
+        self.value -= num
+        s.push(self.value)
+        return self.value
 
     def undo(self):
-        pass
+        if s.isEmpty():
+            return "Nothing to Undo"
+        self.state = s.peek()
+        self.value = self.state
+        s.pop()
+        return s.peek()
 
-    def redo(self):
-        pass
+    def redo(self):  
+        if self.state != 0:
+            s.push(self.state)
+            self.value = self.state
+        else:
+            return "Nothing to redo"
+        return s.peek()
 
     def bulk_undo(self, steps: int):
-        pass
+        if s.isEmpty() :
+            return "Noting to Undo"
+        else:
+            for i in range(steps):
+                self.undo()
 
     def bulk_redo(self, steps: int):
-        pass
+        if s.isEmpty() :
+            return "Noting to Redo"
+        else:
+            for i in range(steps):
+                self.redo()
